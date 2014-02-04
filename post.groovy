4fd1591a6141aec345d1eb4c0c09b21a66f1ac55
Ladislav Prskavec <ladislav@prskavec.net>
2014-02-04 17:19:59 +0100

    Add version with JsonSlurper

--- post.groovy
+++ post.groovy
@@ -1,15 +1,19 @@
 import groovyx.net.http.RESTClient
 import static groovyx.net.http.ContentType.JSON
+import groovy.json.JsonSlurper
 
 url = "http://restapi3.apiary.io"
 
 @Grab (group = 'org.codehaus.groovy.modules.http-builder', module = 'http-builder', version = '0.5.0') 
 def client = new RESTClient(url)
 
+
+def jsonObj = new JsonSlurper().parseText('{ "title": "Buy cheese and bread for breakfast." }')
 def response = client.post(path: "/notes", 
   contentType: JSON,
-  body: [ "title": "Pick-up posters from Post-Office"],
+  body: jsonObj,
   headers: [Accept: 'application/json'])
 
-println("Status:" + response.status)
-println("Body:" + response.data)
+println("Status: " + response.status)
+println("ContentType: " + response.contentType)
+println("Body: " + response.data)
